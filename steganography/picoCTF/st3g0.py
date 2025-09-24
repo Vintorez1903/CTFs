06695dcd92e91d2e274d985259a730ab2ad4392c
Vintorez1903 <aiden.v.melton@gmail.com>
2025-09-23 20:41:29 -0400

    Update st3g0.py

--- steganography/picoCTF/st3g0.py
+++ steganography/picoCTF/st3g0.py
@@ -1,19 +1,1 @@
-import requests
 
-url = "http://mercury.picoctf.net:27177/check"
-
-def sendRequest(i):
-    cookies = {
-        "name": str(i)
-    }
-    
-    response = requests.get(url,cookies=cookies)
-    return response.text.split("<b>")[1].split("\n")[0]
-
-def main():
-        i = 0
-        while i <= 100:
-            print(sendRequest(i))
-            i+=1
-
-main()
